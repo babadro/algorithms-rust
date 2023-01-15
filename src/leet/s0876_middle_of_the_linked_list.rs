42de7b3bb09f22e818a56c0e2dcfe8b0d1fc86af
babadro <babadro@gmail.com>
2023-01-15 08:07:40 +0100

    middle of the linked list

--- src/leet/s0876_middle_of_the_linked_list.rs
+++ src/leet/s0876_middle_of_the_linked_list.rs
@@ -1,17 +1,43 @@
 use crate::helpers::linked_list::ListNode;
+use std::ops::Deref;
 
 struct Solution;
 
 impl Solution {
     pub fn middle_node(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
-        let mut slow = &head;
-        let mut fast = &head;
+        let mut slow = head.as_ref();
+        let mut fast = head.as_ref();
 
-        while fast.is_some() && fast.as_ref().unwrap().next.is_some() {
-            slow = &slow.as_ref().unwrap().next;
-            fast = &fast.as_ref().unwrap().next.as_ref().unwrap().next;
+        while fast.is_some() && fast.unwrap().next.is_some() {
+            slow = slow.unwrap().next.as_ref();
+            fast = fast.unwrap().next.as_ref().unwrap().next.as_ref();
         }
 
-        slow.clone()
+        slow.cloned()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::helpers::linked_list::vector_to_linked_list;
+
+    #[test]
+    fn test_middle_node() {
+        let test_cases = [
+            (vec![0], 0),
+            (vec![0, 1], 1),
+            (vec![0, 1, 2], 1),
+            (vec![0, 1, 2, 3], 2),
+            (vec![0, 1, 2, 3, 4], 2),
+        ];
+
+        for (input, want) in test_cases.iter() {
+            let head = vector_to_linked_list(input);
+
+            let got = Solution::middle_node(head);
+
+            assert_eq!(got.unwrap().val, *want);
+        }
     }
 }
