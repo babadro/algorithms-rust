722ab1fb5a8eccb622d5222b7eb4cad00de88864
babadro <babadro@gmail.com>
2023-03-29 05:59:08 +0200

    reverse nodes in even len groups draft2

--- src/leet/s2074_reverse_nodes_in_even_length_groups.rs
+++ src/leet/s2074_reverse_nodes_in_even_length_groups.rs
@@ -10,36 +10,36 @@ impl Solution {
         let mut prev: &mut Option<Box<ListNode>> = &mut head;
 
         let mut expected_len = 0;
+
         while cur.is_some() {
             expected_len += 1;
-
             let mut actual_len = 0;
-            while let Some(mut n) = cur.take() {
+
+            let mut tmp_cur = &cur;
+            while tmp_cur.is_some() && actual_len < expected_len {
+                tmp_cur = &tmp_cur.as_ref().unwrap().next;
+
+                actual_len += 1
+            }
+
+            if actual_len % 2 == 1 {
+                // todo
+                for _ in 0..actual_len {
+                    cur = cur.unwrap().next;
+                }
+
+                continue;
+            }
+
+            for _ in 0..actual_len {
+                let mut n = cur.take().unwrap();
                 cur = n.next.take();
                 n.next = prev.take();
                 *prev = Some(n);
-                actual_len += 1;
-                if actual_len == expected_len {
-                    break;
-                }
             }
 
-            if actual_len % 2 == 0 {
-                while prev.is_some() {
-                    prev = &mut prev.as_mut().unwrap().next;
-                }
-            } else {
-                let mut new_cur = prev.take();
-                while let Some(mut n) = new_cur.take() {
-                    new_cur = n.next.take();
-                    n.next = prev.take();
-                    *prev = Some(n);
-                }
-
-                if cur.is_some() {
-                    let _ = new_cur.insert(cur.take().unwrap());
-                    cur = new_cur;
-                }
+            for _ in 0..actual_len {
+                prev = &mut prev.as_mut().unwrap().next;
             }
         }
 
@@ -56,8 +56,8 @@ mod tests {
     fn test_reverse_even_length_groups() {
         let test_cases = [
             //(vec![1], vec![1]),
-            //(vec![1, 2], vec![1, 2]),
-            (vec![1, 2, 3], vec![1, 3, 2]),
+            (vec![1, 2], vec![1, 2]),
+            //(vec![1, 2, 3], vec![1, 3, 2]),
             //(vec![1, 2, 3, 4], vec![1, 3, 2, 4]),
             //(vec![1, 2, 3, 4, 5], vec![1, 3, 2, 5, 4]),
             //(vec![1, 2, 3, 4, 5, 6], vec![1, 3, 2, 4, 5, 6]),
